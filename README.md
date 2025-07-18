efe572d92229cbb64688d2ebb57cd7a1cde31732
Muhammed Can Küçükaslan <muhammed.kucukaslan@getir.com>
2025-07-18 23:26:14 +0300

    Add Abseil as a submodule and integrate it into the project

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Reproducing the project step by step
+
+
+
+## Init git submodules and add abseil https://github.com/abseil/abseil-cpp
+
+```bash
+git submodule update --init --recursive
+git submodule add https://github.com/abseil/abseil-cpp
+#git submodule update --remote abseil-cpp
+```
+
+## Add abseil to CMakeLists.txt
+
+```cmake
+add_subdirectory(abseil-cpp)
+target_link_libraries(my-project-exe PRIVATE absl::strings)
+target_include_directories(my-project-exe PRIVATE absl::strings)
+```
--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
+#include "absl/strings/str_cat.h"
+
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
     // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
     auto lang = "C++";
     std::cout << "Hello and welcome to " << lang << "!\n";
 
+
+    // use abseil strings to print a message
+    std::string message = absl::StrCat("This is a message using ", lang, " and Abseil!");
+
+    std::cout << message << std::endl;
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
